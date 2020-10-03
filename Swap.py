5bca367d2754a6a31b36d7d82df41adff17f2ec5
vegetaop <72316289+vegetaop@users.noreply.github.com>
2020-10-04 00:37:04 +0530

    Create Swap.py

---
+++ Swap.py
@@ -0,0 +1,10 @@
+
+x = 5
+y = 7
+  
+print ("Before swapping: ") 
+print("Value of x : ", x, " and y : ", y) 
+x, y = y, x 
+  
+print ("After swapping: ") 
+print("Value of x : ", x, " and y : ", y)
