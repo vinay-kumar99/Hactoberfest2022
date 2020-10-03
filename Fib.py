4cfce4e10e24d8cd8e4685a42a3c35cc3dc59a44
vegetaop <72316289+vegetaop@users.noreply.github.com>
2020-10-04 00:16:37 +0530

    Fibonacci created

---
+++ Fib.py
@@ -0,0 +1,17 @@
+
+def Fibonacci(n):
+	if n<=0:
+		print("Incorrect input")
+	
+	elif n==1:
+		return 0
+	
+	elif n==2:
+		return 1
+	else:
+		return Fibonacci(n-1)+Fibonacci(n-2)
+
+
+
+print(Fibonacci(9))
+
