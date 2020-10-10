18459d84566f201436183ae54f0fa48b73316c21
Amit Sagar <61499207+Amit-Sagar@users.noreply.github.com>
2020-10-10 17:03:59 +0530

    Create sum.c

---
+++ C/sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main(){
+ int x, y ,sum;
+ printf("The value of x is");
+ scanf("%d",&x);
+ printf("The value of y is");
+ scanf("%d",&y);
+ sum = x + y;
+ printf("Sum of two numbers is %d",sum);
+ return 0;
+}
