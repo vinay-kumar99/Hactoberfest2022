1596020d28907d06d0232ebbbd4f9731364146cd
niyatirani <73244666+niyatirani@users.noreply.github.com>
2020-10-21 21:27:46 +0530

    Create K largest Elements.java
    use of priority Queue

---
+++ Java/K largest Elements.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class KlargestElements2 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] a = new int[n];
+		for(int i = 0; i < n; i++) {
+			a[i] = scn.nextInt();
+		}
+		int k = scn.nextInt();
+		PriorityQueue<Integer>	pq = new PriorityQueue<>();
+		for(int i = 0; i < n; i++) {
+			if(i < k) {
+				pq.add(a[i]);
+			}else {
+				if(pq.peek() < a[i]) {
+					pq.remove();
+					pq.add(a[i]);
+				}
+			}
+		}
+		
+		while (pq.size() > 0) {
+			System.out.println(pq.remove());
+		}
+	}
+
+}
