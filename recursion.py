e4ef57e2f067ed89bf9a9e6d57f7b238c63da448
madhuri0201 <73581827+madhuri0201@users.noreply.github.com>
2020-10-28 18:58:41 +0530

    updated  recursion.py

--- recursion.py
+++ recursion.py
@@ -10,3 +10,21 @@ def factorial(x):
 
 num = 3
 print("The factorial of", num, "is", factorial(num))
+
+
+
+
+def recursive_fibonacci(n): 
+   if n <= 1: 
+       return n 
+   else: 
+       return(recursive_fibonacci(n-1) + recursive_fibonacci(n-2)) 
+   
+n_terms = 10
+    
+if n_terms <= 0: 
+   print("Invalid input ! Please input a positive value") 
+else: 
+   print("Fibonacci series:") 
+   for i in range(n_terms): 
+       print(recursive_fibonacci(i))
