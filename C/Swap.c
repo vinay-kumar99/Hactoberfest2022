98ed7ed872f7fe6d6ba061dc6824a8c545a728d0
Vivek Kumar Mishra <68559766+Vivek-K-M@users.noreply.github.com>
2021-10-01 23:22:56 +0530

    Added Efficient swaping of 2 elements
    Please Mark it as a Hacktoberfest 2021 contribution.

--- C/Swap.c
+++ C/Swap.c
@@ -8,11 +8,13 @@ int main()
 	scanf("%d", &x); 
 	printf("\nEnter Value of y "); 
 	scanf("%d", &y); 
-
-	int temp = x; 
-	x = y; 
-	y = temp; 
-
+	
+	//swap without using third variable.
+	//Use only mathematical operations.
+	x = x + y;
+	y = x - y;
+	x = x - y;
+	
 	printf("\nAfter Swapping: x = %d, y = %d", x, y); 
 	return 0; 
 } 
