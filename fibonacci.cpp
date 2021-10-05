b71b939bfd13646aac2c90efd3d77c5e6aacba56
Abhishek raut <56253046+abhishek23012000@users.noreply.github.com>
2021-10-05 23:28:04 +0530

    update fibonacchi.cpp
    reduced the time complexity by using dynamic programming

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,20 +5,18 @@
 #include<iostream>
 using namespace std;
 
-int Fib(int n)
+int fib(int n)
 {
-    int f[n + 2];
+	int f[n + 2];
+	int i;
+	f[0] = 0;
+	f[1] = 1;
 
-    f[0] = 0;
-    f[1] = 1;
-    int i;
- 
-    for(i = 2; i <= n; i++)
-    {
-        f[i] = f[i - 1] + f[i - 2];
-    }
-    
-    return f[n];
+	for(i = 2; i <= n; i++)
+	{
+	f[i] = f[i - 1] + f[i - 2];
+	}
+	return f[n];
 }
 
 int main(){
