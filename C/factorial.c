1f829f28f072a52b11b3c204fe01ab0a80167b88
shaza0q <90998258+shaza0q@users.noreply.github.com>
2021-10-09 15:09:40 +0530

    Update factorial.c

--- C/factorial.c
+++ C/factorial.c
@@ -2,18 +2,19 @@
 int main() {
     int n, i;
     unsigned long long fact = 1;
-    printf("Enter an integer for which you want to find factorial: ");
+l1:    printf("Enter an integer for which you want to find factorial: ");
     scanf("%d", &n);
 
     // shows error if the user enters a negative integer
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-    else {
-        for (i = 1; i <= n; ++i) {
-            fact *= i;
-        }
-        printf("Factorial of %d = %llu", n, fact);
+    if(n<0){
+        printf("Factorial of negative number does not exist\nEnter positive number.\n");
+        goto l1;
     }
-
+    else{
+        for(i=1;i<n;++i){
+            fact*=i;
+        }
+    }
+    printf("Factorial of %d = %llu", n, fact);
     return 0;
 }
