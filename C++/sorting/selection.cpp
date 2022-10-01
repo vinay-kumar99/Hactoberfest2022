5a1dbeb8397e464a913406e588d1a5dbeb6397c7
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2022-10-01 11:26:41 +0530

    Update selection.cpp

--- C++/sorting/selection.cpp
+++ C++/sorting/selection.cpp
@@ -1,41 +1,48 @@
-//Selection Sort;;;
-
 #include <iostream>
-#include <algorithm>
-
 using namespace std;
-
-void selectionSort(int arr[], int n){
-
-    int i , j , imin;
-
-    for (i =0; i< n-1; i++){
-
-        imin = i;
-
-        for (j = i+1; j < n; j++){
-
-            if (arr[j] < arr[imin]){
-
-                imin = j;
-
-                swap(arr[i], arr[imin]);
-
-            }
-        }
-    }
+ 
+void swap(int *a, int *b)
+{
+   int temp = *a;
+   *a = *b;
+   *b = temp;
 }
-
-
-int main() {
-
-    int arr[] = {20,3,52,4};
-    int n = sizeof(arr)/sizeof(arr[0]);
-
-    selectionSort(arr, n);
-    for (int i =0; i < n; i++){
-        cout << arr[i] << " ";
-    }
-
-    return 0;
+ 
+void selectionSort(int arr[], int n)
+{
+   int min_index;
+  
+   for (int i = 0; i < n-1; i++)
+   {
+       min_index = i;
+       for (int j = i+1; j < n; j++)
+       {
+           if (arr[j] < arr[min_index])
+               min_index = j;
+       }
+       swap(&arr[min_index], &arr[i]);
+   }
+}
+ 
+void printArray(int arr[], int n)
+{
+   for (int i = 0; i < n; i++)
+       cout << arr[i] << " ";
+   cout << "\n";
+}
+ 
+int main()
+{
+   int arr[] = {6, 3, 8, 9, 5};
+   int n = sizeof(arr)/sizeof(arr[0]);
+  
+   cout << "Given Array: ";
+   printArray(arr, n);
+  
+   selectionSort(arr, n);
+  
+   cout << "Sorted Array: ";
+   printArray(arr, n);
+  
+   return 0;
 }
